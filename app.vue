c34f971839014b225341cd3eed9bd883d17d94a2
Auriane Labille <auriane.labille@gmail.com>
2023-01-13 13:53:32 +0100

    Update app.vue

--- app.vue
+++ app.vue
@@ -1,10 +1,11 @@
 <template>
-  <div class="container" v-if="!choice1 && !choice2">
-    <h1 class="text-center">Merci pour vos votes !</h1>
-    <h1>Classement</h1>
-    <p v-for="item in leaderboard">{{ item.title }} - {{ item.count }} votes</p>
-  </div>
   <div class="container p-0 m-0">
+    <div class="container" v-if="!choice1 && !choice2">
+        <h1 class="text-center">Merci pour vos votes !</h1>
+        <h1>Classement</h1>
+        <p v-for="item in leaderboard" :key="item">{{ item.title }} - {{ item.count }} votes</p>
+    </div>
+
     <div class="d-flex flex-wrap p-3 gap-3" v-if="choice1 && choice2">
       <img
         :src="choice1.first_choice_image_url"
@@ -23,11 +24,6 @@
       <div class="position-absolute rounded-circle bg-white d-flex justify-content-center align-items-center m-auto border border-primary border-5">
         <h1 class="text-primary mb-0">?</h1>
       </div>
-    </div>
-
-    <div class="container" v-if="!choice1 && !choice2">
-      <h1>Top 10 voted images</h1>
-      
     </div>
   </div>
 </template>
